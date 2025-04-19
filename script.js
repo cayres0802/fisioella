57cf79ab889be6c730035c0ca8a6d8a31b880201
Maria Clara Cayres <0123m.m4567@gmail.com>
2025-04-18 21:26:02 -0400

    atualização

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', () => {
     backToTop.classList.remove('show');
   }
 });
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    const icon = document.querySelector('.hamburger i');
+
+    navbar.classList.toggle('active');
+
+    // Troca ícone
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
+}
+document.querySelectorAll('.navbar a').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelector('.navbar').classList.remove('active');
+        const icon = document.querySelector('.hamburger i');
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-times');
+    });
+});
